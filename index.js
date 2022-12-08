62cdb7e4a3bc865c029c9a6b7d17463fed06b8cf
Grayson Shogan <graysontoddshogan@gmail.com>
2022-12-08 14:51:23 -0700

    Answered rockPaperScissors

--- index.js
+++ index.js
@@ -184,8 +184,28 @@ Use the game function below to do the following:
 RULES OF THE GAME: Scissors beats Paper | Paper beats Rock | Rock beats Scissors | Or there's a tie
 */
 
+let computer = Math.floor(Math.random() * 3);
+
+if (computer < 0.34){
+  computer = 'rock';
+} else if (computer <= 0.67){
+  computer = 'paper';
+} else if (computer > 0.67){
+  computer = 'scissors';
+}
+
 function game(user, computer){
-  /*add your code here*/
+  if (user === computer){
+    return `it's a tie`;
+  } else if (user === 'rock' && computer === 'scissors'){
+    return `you win!`;
+  } else if (user === 'scissors' && computer === 'paper'){
+    return `you win!`;
+  } else if (user === 'paper' && computer === 'rock'){
+    return `you win!`;
+  } else {
+    return "you lose!";
+  }
 }
 
 
